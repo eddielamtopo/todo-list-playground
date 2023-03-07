a3c070bd1711ddd91c62fdbc460c6331445cc61b
Eddie Lam <eddielam@Eddies-MacBook-Pro.local>
2023-03-07 17:09:03 +0800

    refactor: rename form binding events drtails maps folder name

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,7 @@ import {AbstractFieldDirective} from './view/helpers/abstract-field-directive';
 import {
   standardFormBindingMap,
   vaadinFormBindingEventMap,
-} from './view/helpers/form-event-binding-details-maps';
+} from './view/helpers/form-binding-events-details-maps';
 
 console.log('running main.ts...');
 
